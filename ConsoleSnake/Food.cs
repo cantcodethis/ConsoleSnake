5bcd871be9124419dcda9714a7f6ae9d7cc80ada
cantcodethis <cantcodethis@gmail.com>
2014-01-07 16:36:55 +0300

    First working commit

---
+++ ConsoleSnake/Food.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleSnake
+{
+    public class Food
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Food()
+        {
+            Random random = new Random();
+
+            int x, y;
+            do
+            {
+                x = random.Next(2, Program.HORIZONTAL_LINE.Length - 1); 
+                y = random.Next(2, Program.VERTICAL_LINES - 1);
+            }
+            while (!CheckSnake(x, y));
+
+            X = x;
+            Y = y;
+
+        }
+
+        private bool CheckSnake(int x, int y)
+        {
+            bool result = true;
+
+            foreach (var snakePart in Program.Snake)
+            {
+                if (snakePart.X == x & snakePart.Y == y)
+                    result = false;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ ConsoleSnake/SnakePart.cs
@@ -0,0 +1,21 @@
+﻿
+namespace ConsoleSnake
+{
+    public class SnakePart
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public SnakePart()
+        {
+            X = 0;
+            Y = 0;
+        }
+
+        public SnakePart(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
